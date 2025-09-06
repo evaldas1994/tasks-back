423ec50cd9f7f73782c85bc288fb420ff866c885
evald <evaldas.tuleikis@gmail.com>
2025-09-06 19:11:49 +0300

    Pridėtas Kernel failas

---
+++ app/Console/Kernel.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Console;
+
+use App\Jobs\Task\CreatePeriodicTasksJob;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Schedule the application's commands.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        $schedule->call(function () {
+            CreatePeriodicTasksJob::dispatch();
+        })->everyMinute();
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__ . '/Commands');
+        require base_path('routes/console.php');
+    }
+}
