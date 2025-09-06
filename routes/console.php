fca0863a71053a7d53df87f7d54496a7eca990ce
evald <evaldas.tuleikis@gmail.com>
2025-09-06 20:26:44 +0300

    Pridėtas RateLimiter

--- routes/console.php
+++ routes/console.php
@@ -5,6 +5,13 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+
+RateLimiter::for('api', function ($request) {
+    return Limit::perMinute(60)->by($request->ip());
+});
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
