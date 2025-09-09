562bc24b437b9fbb0b054e81a19cf1f39d64b92f
evald <evaldas.tuleikis@gmail.com>
2025-09-09 06:07:58 +0300

    Pridėjau NoCache middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\NoCache;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
             EnsureFrontendRequestsAreStateful::class,
 //            'throttle:api',
             SubstituteBindings::class,
+            NoCache::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
