995517ad7e624ba00497015dfc9b73135ec0ac62
evald <evaldas.tuleikis@gmail.com>
2025-09-15 19:56:12 +0300

    Užduotys dviems dienoms

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:5173', 'http://192.168.1.3:5174','http://192.168.1.3:5175', 'https://ulala.titobu.eu', 'https://front.titobu.eu/'],
 //    'allowed_origins' => ['*'],
