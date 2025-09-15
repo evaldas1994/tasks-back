5774db5f9cd7f506f25039087295f5a08b7f6bdc
evald <evaldas.tuleikis@gmail.com>
2025-09-15 20:02:46 +0300

    Užduotys dviems dienoms

--- config/cors.php
+++ config/cors.php
@@ -3,8 +3,13 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://192.168.1.3:5174','http://192.168.1.3:5175', 'https://ulala.titobu.eu', 'https://front.titobu.eu/'],
-//    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://192.168.1.3:5174',
+        'http://192.168.1.3:5175',
+        'https://ulala.titobu.eu',
+        'https://front.titobu.eu', // <-- be trailing slash
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
